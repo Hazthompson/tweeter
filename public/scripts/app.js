eb75331aae25555784a0d2b93a170e0a46ee3ab8
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-05-15 20:42:16 +0000

    styles the container-tweet section  with hover

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,15 @@
+$(document).ready(function() {
+
+  $('.container-tweets').hover(
+       function(){ $(this).addClass('hover') },
+       function(){ $(this).removeClass('hover') }
+)
+
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+});
