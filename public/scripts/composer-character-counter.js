369fe336a6987f4683e0594b6ea375a280e986a1
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-05-15 05:45:50 +0000

    added character-counter js. which counts down characters in tweet and impliments CSS to turn red when negative

---
+++ public/scripts/composer-character-counter.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  let textarea = $(".new-tweet textarea");
+
+  textarea.on("input", function(event) {
+    console.log($(this));
+
+    let targetedForm = $(this).parent();
+    let targetedCounter = $(targetedForm.find(".counter")); //already jquerified thing then don't need dollar here again?
+
+    let counterValue = 140 - $(this).val().length;
+    targetedCounter.html(counterValue);
+
+    if (counterValue < 0) {
+      targetedCounter.addClass("negative");
+    } else {
+      targetedCounter.removeClass("negative");
+    }
+  });
+
+  //wrapping it in the dollar and the brackets attaches the power of the jquery library and all the function that come iwth it
+  // plain dom element: this.value.length
+  //Jqueried element (with library) $(this).val().length).
+});
